3910281f094c43c01c01fa3a27f1f92da4c7f532
elmar <xomchenkov91@bk.ru>
2024-10-20 15:18:54 +0500

    Quiz section делается еще

---
+++ src/js/_components.js
@@ -0,0 +1,4 @@
+//console.log("components");
+import "./components/range.js";
+import "./components/catalogProp.js";
+import "./components/guiz.js";
---
+++ src/js/components/catalogProp.js
@@ -0,0 +1,11 @@
+const items = document.querySelectorAll(".catalog-prop__inner .sizes-table tr");
+
+items.forEach((item) => {
+  console.log(item);
+  item.addEventListener("click", (e) => {
+    if (e.target.tagName === "TD") {
+      console.log(e.target);
+      e.target.classList.toggle("td-active");
+    }
+  });
+});
